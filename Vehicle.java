1973fc5713f36d44ea398f65accde093ef989442
=ramkrushna <you@example.com>
2025-04-28 20:21:22 +0530

    parkingLot lld

---
+++ Vehicle.java
@@ -0,0 +1,13 @@
+public abstract class Vehicle {
+    protected String licensePlate;
+    protected VehicleType type;
+
+    public Vehicle(String licensePlate, VehicleType type) {
+        this.licensePlate = licensePlate;
+        this.type = type;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+}
